04ebde69e463e90faedff8c9d95cbb5a4160f065
andrijast <andrija.stevanovic56@gmail.com>
2024-09-23 07:21:31 +0200

    Tokenization

--- src/Lexing.ts
+++ src/Lexing.ts
@@ -1,20 +1,59 @@
 import type { unicode_string } from "./utility";
+import { HuffmanTree } from "./HuffmanTree";
 
-function normalize(word: unicode_string): unicode_string {
-    return word.toLowerCase();
+export type token_data = {
+    word: unicode_string,
+    cap?: boolean, // capitalized
+    acap?: boolean, // all caps
+};
+
+const hm = new HuffmanTree();
+
+function splits(str: unicode_string): token_data[] {
+    if (str === "") return [];
+    for (let i = str.length; i > 0; i--) {
+        const word = str.slice(0, i);
+        const norm = word.toLowerCase();
+        if (!hm.check(norm)) continue;
+        if (word === norm)
+            return [{word: norm}, ...splits(str.slice(i))]
+        if (word === norm.charAt(0).toUpperCase() + norm.slice(1))
+            return [{word: norm, cap: true}, ...splits(str.slice(i))]
+        if (word === norm.toUpperCase())
+            return [{word: norm, acap: true}, ...splits(str.slice(i))]
+    }
+    return [{word: str}];
 }
 
-export function tokenize(text: unicode_string): unicode_string[] {
+export function tokenize(text: unicode_string): token_data[] {
 
-    return text.split(' ')
-            //    .map(x => normalize(x))
-               .filter(x => x != "")
+    let ret: token_data[] = []
 
+    const all = text.split(' ');
+    let first = true;
+
+    for (const str of all) {
+
+        if (!first) ret.push({word: " "});
+        first = false;
+
+        ret = [...ret, ...splits(str)];
+    }
+
+    return ret;
 }
 
 
-export function stylize(words: unicode_string[]): unicode_string {
+export function stylize(tokens: token_data[]): unicode_string {
 
-    return words.join(' ');
+    let ret = "";
+    for (const token of tokens) {
+        let word = token.word
+        if (token.cap) word = word.charAt(0).toUpperCase() + word.slice(1);
+        if (token.acap) word = word.toUpperCase();
+        ret += word
+    }
+
+    return ret;
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,75 @@
 import type { byte_array, dict_entry, unicode_string } from "./utility";
 import { BinaryStream, debug } from "./utility";
 import { pack_data, unpack_data } from "./Format";
-import { tokenize, stylize } from "./Lexing";
+import { tokenize, stylize, type token_data } from "./Lexing";
 import { HuffmanTree, full_dict, combineDicts } from "./HuffmanTree";
 
+function encode_header(word: token_data): string {
+    if (word.cap) return "10";
+    if (word.acap) return "11";
+    else return "0";
+}
+
+function decode_header(bs: BinaryStream): token_data | null {
+    let ret = {word: "----"};
+    let x;
+    x = bs.take(); if (!x) return null;
+    if (x === "0") return ret;
+    x = bs.take(); if (!x) return null;
+    if (x === "0") return {...ret, cap: true};
+    if (x === "1") return {...ret, acap: true};
+    return null;
+}
 
 export function encode(text: unicode_string): byte_array {
 
-    const words = tokenize(text);
+    // tokenization
+    const tokens = tokenize(text);
+    debug("tokens", tokens.map(tok => tok.word));
+    debug("tokens_full", tokens.slice(0, 1000));
 
+    // collecting additions
     const hm1 = new HuffmanTree();
-    const additions = new Set<string>();
+    const additions_set = new Set<string>();
     let max_rank = 0;
-    for (const word of words) {
-        const rank = hm1.check(word);
+    for (const token of tokens) {
+        const rank = hm1.check(token.word);
         if (!rank) {
-            additions.add(word);
+            additions_set.add(token.word);
         } else {
             max_rank = Math.max(max_rank, rank);
         }
     }
+    const additions = [...additions_set];
 
+    // max_rank and appendix
+    max_rank = Math.min(max_rank, 65535);
     const appendix: dict_entry[] = [];
-    [...additions].forEach(word => {
+    additions.forEach(word => {
         appendix.push({word: word, freq: 1_000_000})
     })
     const dict = combineDicts(full_dict.slice(0, max_rank), appendix);
-    // console.log(additions)
 
-    max_rank = Math.min(max_rank, 65535);
+    debug("max_rank", max_rank);
+    debug("additions", additions);
 
+
+    // constructing coding sequence
     const hm2 = new HuffmanTree(dict);
     const ret = new BinaryStream();
-
-    for (const word of words) {
-        const code = hm2.encode(word)
-        if (code)
-            ret.appendString(code)
-        else throw new Error(`what happened?? ${word}`)
+    for (const word of tokens) {
+        const head = encode_header(word);
+        const code = hm2.encode(word.word)
+        if (!code) throw new Error(`what happened?? ${word}`);
+        ret.appendString(head)
+        ret.appendString(code)
     }
 
-    // console.log(additions);
-    // console.log(max_rank);
-
-    return pack_data([...additions], max_rank, ret.toBytes());
+    // pack up
+    return pack_data(additions, max_rank, ret.toBytes());
 }
 
-function decode_header(bs: BinaryStream): number {
-    return 0;
-}
-
-export function decode(cipher: byte_array): unicode_string | null {
+export function decode(cipher: byte_array): unicode_string {
 
     const [additions, dict_sz, coding] = unpack_data(cipher);
 
@@ -61,29 +80,23 @@ export function decode(cipher: byte_array): unicode_string | null {
 
     const hm = new HuffmanTree(dict);
 
-    const words: unicode_string[] = [];
+    const tokens: token_data[] = [];
 
-    try {
+    const bs = new BinaryStream(coding);
 
-        const bs = new BinaryStream(coding);
+    while (true) {
         const which = decode_header(bs);
-        switch (which) {
-            case 0: {
-                const word = hm.decode(bs);
-                if (word)
-                    words.push(word);
-            }
+        const word = hm.decode(bs);
+        if (!which || !word) {
+            if (bs.take() !== null) throw new Error("decoding error: huffman decode");
+            else break; // either empty or padding bits
         }
-        const ret = stylize(words);
-
-        return ret;
-
-    } catch (err) {
-
-        console.log("DECODING FAILED!");
-        return null;
-
+        tokens.push({...which, word: word});
     }
+
+    const ret = stylize(tokens);
+
+    return ret;
 
 
 }
@@ -105,6 +118,8 @@ async function run(input: unicode_string) {
     const dec_sz = new Blob([dec]).size
     console.log(`decompressed size: ${dec_sz}`)
     debug("decompressed", dec);
+
+    console.log("correctness:", dec === input);
 
 }
 
